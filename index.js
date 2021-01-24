9f4393eb18c132272291702a8801fbde000684eb
Александр Каплунов <kaplunov27@gmail.com>
2021-01-24 09:18:32 +0300

    Добавил счеткик записей

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const input = document.querySelector('#input-main');
 const ul = document.querySelector('.list');
 let li = document.querySelectorAll('.list li');
+let tasks = [];
 
 input.addEventListener('keypress', function (event) {
   if (input.value === '') {
@@ -29,25 +30,41 @@ let newTask = () => {
     span.className = 'close';
     span.appendChild(spanText);
     link.appendChild(span);
+
     let filter = document.querySelector('.filter-list');
     filter.style.display = 'flex';
-    span.addEventListener('click', removeTask);
 
-    //Чекбокс
+    //Чекбоксы
     let checkbox = document.createElement('input');
-
     checkbox.type = 'checkbox';
     checkbox.id = 'checkbox-id';
-    // checkbox.className = 'checkbox-class';
-    // checkbox.checked = false;
     link.appendChild(checkbox);
     ul.appendChild(link);
+    //Всего задач
+    let allTask = document.querySelector('.all-count'),
+      list = document.querySelectorAll('.list > a');
+    tasks.push(list);
+
+    // -------------- Дописано:
+    updateCount();
+
+    //Функция удаления задачи
+    span.addEventListener('click', function (event) {
+      if (event.target.tagName === 'SPAN') {
+        let close = event.target.parentNode;
+        close.remove();
+
+        // -------------- Дописано:
+        tasks.splice(tasks.indexOf(list), 1);
+        updateCount();
+      }
+    });
   }
 };
 
-let removeTask = (event) => {
-  if (event.target.tagName === 'SPAN') {
-    let close = event.target.parentNode;
-    close.remove();
-  }
-};
+// -------------- Дописано:
+function updateCount() {
+  let allTask = document.querySelector('.all-count');
+  let count = tasks.length;
+  allTask.textContent = `Всего задач: ${[count]}`;
+}
